f60014e4576d1bc2d74489e93e522e66d1343db5
= <pjfritch1@gmail.com>
2017-10-19 12:38:18 -0700

    add title

--- bundle.js
+++ bundle.js
@@ -60,7 +60,7 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ 	return __webpack_require__(__webpack_require__.s = 1);
 /******/ })
 /************************************************************************/
 /******/ ([
@@ -68,8 +68,158 @@
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__link__ = __webpack_require__(3);
+
+
+const canvas = document.getElementById("canvas");
+// // // console.log("canvas",canvas);
+const ctx = canvas.getContext("2d");
+
+class Point {
+  constructor(options) {
+    // // console.log("options:", options);
+    this.lastX = options.lastX;
+    this.lastY = options.lastY;
+    this.nextX = options.nextX;
+    this.nextY = options.nextY;
+    this.position = options.position;
+    // // console.log("pointPosition:", this.position);
+    this.velocity = options.velocity;
+    this.mass = options.mass;
+    this.radius = options.radius;
+    this.area = (Math.PI * this.radius * this.radius) / 10000;
+    this.pinned = options.pinned || false;
+    this.aX = 0;
+    if (options.aY) {
+      this.aY = options.aY;
+    } else {
+      this.aY = 20.81;
+    }
+    this.links = options.links || [];
+    if (options.isAnchor) {
+      this.isAnchor = options.isAnchor;
+    } else {
+      this.isAnchor = false;
+    }
+
+    this.updatePos = this.updatePos.bind(this);
+    this.collideWith = this.collideWith.bind(this);
+    this.applyForce = this.applyForce.bind(this);
+    this.solveLinkConstraints = this.solveLinkConstraints.bind(this);
+    this.addLinkTo = this.addLinkTo.bind(this);
+    this.removeLink = this.removeLink.bind(this);
+    this.render = this.render.bind(this);
+  }
+
+  updatePos(timeElapsed) {
+    // this.applyForce({x: 0, y: })
+    const seconds = timeElapsed / 100;
+    // // console.log("seconds", seconds);
+    // // console.log("this.aX", this.aX);
+
+    let deltaX = this.position.x - this.lastX;
+    let deltaY = this.position.y - this.lastY;
+
+    // damping velocity
+    deltaX *= .95;
+    deltaY *= .95;
+    // // console.log("this.position.x", this.position.x);
+    // // console.log("acc component", (0.5 * this.aX * seconds * seconds));
+    this.nextX = this.position.x + deltaX + (0.5 * this.aX * seconds * seconds);
+    this.nextY = this.position.y + deltaY + (0.5 * this.aY * seconds * seconds);
+    // // console.log("NextX:", this.nextX);
+
+
+
+    this.lastX = this.position.x;
+    this.lastY = this.position.y;
+
+    if (!this.pinned) {
+      this.position.x = this.nextX;
+      this.position.y = this.nextY;
+    } else {
+      this.nextX = this.position.x;
+      this.nextY = this.position.y;
+    }
+  }
+
+  collideWith(pt2) {
+    if (!this.pinned) {
+      this.position.x = this.lastX;
+      this.position.y = this.lastY;
+    }
+  }
+
+  // collideWithLink(link) {
+  //   // // console.log("collideLink:", link);
+  //   debugger;
+  //   link.point1.pinned = true;
+  //   // link.point1.position.x = 1;
+  //   // link.point1.position.y = 1;
+  //   link.point2.pinned = true;
+  // }
+
+  applyForce(force) {
+    this.aX += force.x / this.mass;
+    this.aY += force.y / this.mass;
+  }
+
+  solveLinkConstraints() {
+    for (let i = 0; i < this.links.length; i++) {
+      this.links[i].solve();
+    }
+
+
+  }
+
+  addLinkTo(options) {
+    const otherPoint = options.otherPoint;
+    const restingDistance = options.restingDistance;
+    // debugger;
+    const newLink = new __WEBPACK_IMPORTED_MODULE_0__link__["a" /* default */]({point1: this, point2: otherPoint, restingDistance});
+    // // console.log("newLink:", newLink);
+    this.links.push(newLink);
+    otherPoint.links.push(newLink);
+    // // console.log("links:", this.links);
+  }
+
+  removeLink(link) {
+    this.links.splice(this.links.indexOf(link), 1);
+  }
+
+  render() {
+    // debugger;
+    // ctx.translate(point.position.x, point.position.y);
+    // ctx.clearRect(0,0,width, height);
+    if (this.isAnchor) {
+      ctx.fillStyle = 'black';
+      ctx.strokeStyle = '#000000';
+      ctx.beginPath();
+      ctx.arc(this.position.x, this.position.y, this.radius * 5, 0, Math.PI*2, true);
+      ctx.fill();
+      ctx.closePath();
+    }
+
+    const links = this.links;
+
+    if (links.length > 0) {
+      for (let i = 0; i < links.length; i++) {
+        links[i].render();
+      }
+    }
+  }
+}
+
+/* harmony default export */ __webpack_exports__["a"] = (Point);
+
+
+/***/ }),
+/* 1 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
 Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__physics_js__ = __webpack_require__(1);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__physics_js__ = __webpack_require__(2);
 
 
 
@@ -85,11 +235,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 /***/ }),
-/* 1 */
+/* 2 */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__point__ = __webpack_require__(2);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__point__ = __webpack_require__(0);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__seed_data__ = __webpack_require__(4);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__body__ = __webpack_require__(5);
 
@@ -446,156 +596,6 @@ animate();
 
 
 /***/ }),
-/* 2 */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__link__ = __webpack_require__(3);
-
-
-const canvas = document.getElementById("canvas");
-// // // console.log("canvas",canvas);
-const ctx = canvas.getContext("2d");
-
-class Point {
-  constructor(options) {
-    // // console.log("options:", options);
-    this.lastX = options.lastX;
-    this.lastY = options.lastY;
-    this.nextX = options.nextX;
-    this.nextY = options.nextY;
-    this.position = options.position;
-    // // console.log("pointPosition:", this.position);
-    this.velocity = options.velocity;
-    this.mass = options.mass;
-    this.radius = options.radius;
-    this.area = (Math.PI * this.radius * this.radius) / 10000;
-    this.pinned = options.pinned || false;
-    this.aX = 0;
-    if (options.aY) {
-      this.aY = options.aY;
-    } else {
-      this.aY = 20.81;
-    }
-    this.links = options.links || [];
-    if (options.isAnchor) {
-      this.isAnchor = options.isAnchor;
-    } else {
-      this.isAnchor = false;
-    }
-
-    this.updatePos = this.updatePos.bind(this);
-    this.collideWith = this.collideWith.bind(this);
-    this.applyForce = this.applyForce.bind(this);
-    this.solveLinkConstraints = this.solveLinkConstraints.bind(this);
-    this.addLinkTo = this.addLinkTo.bind(this);
-    this.removeLink = this.removeLink.bind(this);
-    this.render = this.render.bind(this);
-  }
-
-  updatePos(timeElapsed) {
-    // this.applyForce({x: 0, y: })
-    const seconds = timeElapsed / 100;
-    // // console.log("seconds", seconds);
-    // // console.log("this.aX", this.aX);
-
-    let deltaX = this.position.x - this.lastX;
-    let deltaY = this.position.y - this.lastY;
-
-    // damping velocity
-    deltaX *= .95;
-    deltaY *= .95;
-    // // console.log("this.position.x", this.position.x);
-    // // console.log("acc component", (0.5 * this.aX * seconds * seconds));
-    this.nextX = this.position.x + deltaX + (0.5 * this.aX * seconds * seconds);
-    this.nextY = this.position.y + deltaY + (0.5 * this.aY * seconds * seconds);
-    // // console.log("NextX:", this.nextX);
-
-
-
-    this.lastX = this.position.x;
-    this.lastY = this.position.y;
-
-    if (!this.pinned) {
-      this.position.x = this.nextX;
-      this.position.y = this.nextY;
-    } else {
-      this.nextX = this.position.x;
-      this.nextY = this.position.y;
-    }
-  }
-
-  collideWith(pt2) {
-    if (!this.pinned) {
-      this.position.x = this.lastX;
-      this.position.y = this.lastY;
-    }
-  }
-
-  // collideWithLink(link) {
-  //   // // console.log("collideLink:", link);
-  //   debugger;
-  //   link.point1.pinned = true;
-  //   // link.point1.position.x = 1;
-  //   // link.point1.position.y = 1;
-  //   link.point2.pinned = true;
-  // }
-
-  applyForce(force) {
-    this.aX += force.x / this.mass;
-    this.aY += force.y / this.mass;
-  }
-
-  solveLinkConstraints() {
-    for (let i = 0; i < this.links.length; i++) {
-      this.links[i].solve();
-    }
-
-
-  }
-
-  addLinkTo(options) {
-    const otherPoint = options.otherPoint;
-    const restingDistance = options.restingDistance;
-    // debugger;
-    const newLink = new __WEBPACK_IMPORTED_MODULE_0__link__["a" /* default */]({point1: this, point2: otherPoint, restingDistance});
-    // // console.log("newLink:", newLink);
-    this.links.push(newLink);
-    otherPoint.links.push(newLink);
-    // // console.log("links:", this.links);
-  }
-
-  removeLink(link) {
-    this.links.splice(this.links.indexOf(link), 1);
-  }
-
-  render() {
-    // debugger;
-    // ctx.translate(point.position.x, point.position.y);
-    // ctx.clearRect(0,0,width, height);
-    if (this.isAnchor) {
-      ctx.fillStyle = 'black';
-      ctx.strokeStyle = '#000000';
-      ctx.beginPath();
-      ctx.arc(this.position.x, this.position.y, this.radius * 5, 0, Math.PI*2, true);
-      ctx.fill();
-      ctx.closePath();
-    }
-
-    const links = this.links;
-
-    if (links.length > 0) {
-      for (let i = 0; i < links.length; i++) {
-        links[i].render();
-      }
-    }
-  }
-}
-
-/* harmony default export */ __webpack_exports__["a"] = (Point);
-
-
-/***/ }),
 /* 3 */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
@@ -693,7 +693,7 @@ class SeedData {
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__point__ = __webpack_require__(2);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__point__ = __webpack_require__(0);
 
 
 class Body {
